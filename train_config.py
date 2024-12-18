3cea680a8431a4b82dc62714dc9519f345f2beae
Al3xSelivanov <138496610+Al3xSelivanov@users.noreply.github.com>
2024-12-18 10:52:59 +0300

    Add files via upload
    Refactored model training logics into script
    Parameters of model are now in 
    Added requirements.txt

---
+++ train_config.py
@@ -0,0 +1,9 @@
+bucket_name = "datasets"
+file_name = "dataset_yolo.zip"
+destination = "./dataset_yolo.zip"
+
+model_name = "yolo11n.yaml"     # ".yaml" is new model, ".pt" is pretrained model
+image_size = (768, 384)
+
+epochs = 25
+confidence = 0.5
