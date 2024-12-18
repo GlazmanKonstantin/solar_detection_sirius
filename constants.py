109070b5eeb2473a86bc27e48e00b6e8abb14479
Artronis <anrelium@gmail.com>
2024-12-18 12:25:06 +0300

    Add constants

---
+++ constants.py
@@ -0,0 +1,22 @@
+IMGSZ = 768
+Y_IMGSZ = 384
+DIGITIZATION_SIZE = 12638
+SEGMENT_COUNT = 20
+
+# Image constants
+DPI = 400
+SMALL_SIZE = 20
+MEDIUM_SIZE = 22
+BIG_SIZE = 24
+FIGSIZE = (10, 7)
+CM_FIGSIZE = (8.5, 7)
+
+# As of 2022, in meters
+SOLAR_RADIUS = 6.957e8
+# Calculated as mean of May 2020 and May 2024, good enough for a rough estimation
+MEAN_RSUN = 1583.2424282224026
+STD_RSUN = 2.947125380936948
+
+# For images
+BASE_URL = "http://127.0.0.1:8000"
+PATH = r"C:\Users\user\Downloads\train\images"
