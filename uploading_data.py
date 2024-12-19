2bc82b5b51295ac5ab40633263fb9687254f8d1b
Al3xSelivanov <138496610+Al3xSelivanov@users.noreply.github.com>
2024-12-19 11:54:20 +0300

    Add uploading_data.py
    a script that goes to minio.smtornado and downloads some files it never seen before, then gives it to model and saves to directory

---
+++ uploading_data.py
@@ -0,0 +1,54 @@
+import minio
+from constants import DOWNLOAD_LIMIT
+import os
+from converter import FitsConverter, YOLOConverter
+from ultralytics import YOLO
+
+
+def clear(directory):
+    for __filename in os.listdir(directory):
+      if not os.path.isdir(os.path.join(directory, __filename)):
+        os.remove(os.path.join(directory, __filename))
+
+
+client = minio.Minio(
+    endpoint="minio.smtornado.ru",
+    access_key=None,
+    secret_key=None,
+    secure=True
+)
+
+downloaded = 0
+fits_bucket = "fits"
+destination = "images/converter_input"
+for element in client.list_objects(bucket_name="fits", recursive=True):
+    if downloaded >= DOWNLOAD_LIMIT:
+        break
+    if element.is_dir:
+        continue
+    filename = element.object_name[41:]
+    if element.object_name.endswith(".fits") and filename not in os.listdir(destination):
+        try:
+            client.fget_object("fits", element.object_name, os.path.join(destination, filename))
+            print(f"File '{element.object_name}' downloaded successfully to '{destination}'.")
+            downloaded += 1
+        except Exception as err:
+            print(f"Error occurred: {err}")
+
+converter_inp_dir = "images/converter_input"
+model_inp_dir = "images/model_input"
+tmp_dir = "images/tmp"
+
+FitsConverter(converter_inp_dir, tmp_dir).convert()
+YOLOConverter(tmp_dir, model_inp_dir).convert()
+clear(tmp_dir)
+
+model = YOLO("runs/detect/train/weights/best.pt")
+model_out_directory = "images/model_output/"
+for filename in os.listdir(model_inp_dir):
+    if filename.endswith(".png"):
+        result = model(os.path.join(model_inp_dir, filename))
+        for r in result:
+            r.save(os.path.join(model_out_directory, filename))
+
+clear(model_inp_dir)
