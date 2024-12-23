e7bf5203556ebdc9fd74c3d443dc547d2711861e
GlazmanKonstantin <glk01012020@gmail.com>
2024-12-23 14:08:55 +0400

    Update seg.py

--- seg.py
+++ seg.py
@@ -11,8 +11,10 @@ from converter import FitsConverter
 import shutil
 from fnmatch import fnmatch
 from math import pi, degrees, sin, cos
+from constants import DIGITIZATION_SIZE, IMGSZ, SEGMENT_COUNT
+import numpy as np
 
-PATH_FITS = r'C:\Users\user\Downloads\hh.fits'
+PATH_FITS = r'C:\Users\user\Downloads\image.fits'
 
 PATH_OUTPUT_SUN = r'C:\Users\user\Downloads'
 PATH_FOLDER = r'C:\Users\user\Downloads\test'
@@ -31,103 +33,68 @@ def get_len(fits_file):
         os.remove(f)
     return len_line
 
+def get_start(path_file):
+    file = open(path_file)
+    s = file.name
+    seg_start = np.arange(SEGMENT_COUNT) * (
+        DIGITIZATION_SIZE // SEGMENT_COUNT
+    )
+    s = s.split('_')[1]
+    s = s.split('.txt')[0]
+    return seg_start[int(s)]
+
 def make_seg(yolo_predict, fits_file, color = "red"):
     len_line = get_len(fits_file)
+    x1 = get_start(yolo_predict)
+    x2 = x1 + IMGSZ - 1
     widthBBox = 2
     head = fits_file[1].header
     RSUN = head["R_SUN"]
     sun_x_c = head["CRPIX1"]
     sun_y_c = head["CRPIX2"]
-
-    x_len = X2 - X1
-    if X1 > X2:
-        x_len = 1 - X2 + X1
-    
-    X_c = 0.0
-    Y_c = 0.0
-    W = 0.0
-    H = 0.0
     with open(yolo_predict) as txt_LINE:
         LINE = txt_LINE.readline()
         
-        X_c, Y_c, W, H = [float(x) for x in LINE.split()[1:]]
-    
-    x_rel1 = X_c - W / 2
-    x_rel2 = X_c + W / 2
-
-    isThree = False
-    if x_rel1 < 0:
-        x_rel1 += 1
-        isThree = True
-    if x_rel2 > 1:
-        x_rel2 -= 1
-        isThree = True
+        X_center, Y_center, W, H = [float(x) for x in LINE.split()[1:]]
+        x0 = X_center - W / 2
+        x1 = X_center + W / 2
         
-    x_1 = X1 + x_rel1 * x_len
-    x_2 = X2 + x_rel2 * x_len
-
-    a__st = x_1 * 2 * pi
-    a__fi = x_2 * 2 * pi
-    
-    x_cir_1 = 1 - x_1
-    x_cir_2 = 1 - x_2
+        a_st = x0 * 2 * pi - pi / 2
+        a_fi = x1 * 2 * pi - pi / 2
+        if a_st < 0:
+            a_st += 2 * pi
+        if a_fi < 0:
+            a_fi += 2 * pi
         
-    a_st = x_cir_1 * 2 * pi
-    a_fi = x_cir_2 * 2 * pi
+        
+        y0 = Y_center - H / 2
+        y1 = Y_center + H / 2
 
-    y_rel1 = Y_c - H / 2
-    y_rel2 = Y_c + H / 2
-
-    y1 = y_rel1 * dR
-    y2 = y_rel2 * dR
-
-    r_st = RSUN + y1
-    r_fi = RSUN + y2
-    
+        y_st = y0
+        y_fi = y1
+        
+        r_st = RSUN + y_st
+        r_fi = RSUN + y_fi
+     
     im_SUN= Image.fromarray(fits_file[1].data).convert('RGB')
     draw = ImageDraw.Draw(im_SUN)
     
     #print(im_SUN.size)
-
-    if isThree:
-        draw.arc((sun_x_c - r_st,
-                  sun_y_c - r_st,
-                  sun_x_c + r_st,
-                  sun_y_c + r_st), degrees(a_st), 360, color, widthBBox)
-        draw.arc((sun_x_c - r_st,
-                  sun_y_c - r_st,
-                  sun_x_c + r_st,
-                  sun_y_c + r_st), 0, degrees(a_fi), color, widthBBox)
-    else:
-        draw.arc((sun_x_c - r_st,
-                  sun_y_c - r_st,
-                  sun_x_c + r_st,
-                  sun_y_c + r_st), degrees(a_st), degrees(a_fi), color, widthBBox)
     
-    if isThree:
-        draw.arc((sun_x_c - r_fi,
-                  sun_y_c - r_fi,
-                  sun_x_c + r_fi,
-                  sun_y_c + r_fi), degrees(a_st), 360, color, widthBBox)
-        draw.arc((sun_x_c - r_fi,
-                  sun_y_c - r_fi,
-                  sun_x_c + r_fi,
-                  sun_y_c + r_fi), 0, degrees(a_fi), color, widthBBox)
-    else:
-        draw.arc((sun_x_c - r_fi,
-                  sun_y_c - r_fi,
-                  sun_x_c + r_fi,
-                  sun_y_c + r_fi), degrees(a_st), degrees(a_fi), color, widthBBox)
+    draw.arc((sun_x_c - r_st, sun_y_c - r_st, sun_x_c + r_st, sun_y_c + r_st), degrees(a_st), degrees(a_fi), color, widthBBox)
+    draw.arc((sun_x_c - r_fi, sun_y_c - r_fi, sun_x_c + r_fi, sun_y_c + r_fi), degrees(a_st), degrees(a_fi), color, widthBBox)
     
-    
+    a_st += pi / 2
+    a_fi += pi / 2
     draw.line([(sun_x_c + r_st * sin(a_st),
                 sun_y_c - r_st * cos(a_st)),
-               (sun_x_c + r_fi * sin(a_st),
+                (sun_x_c + r_fi * sin(a_st),
                 sun_y_c - r_fi * cos(a_st)) ], color, widthBBox)
     draw.line([(sun_x_c + r_st * sin(a_fi),
                 sun_y_c - r_st * cos(a_fi)),
-               (sun_x_c + r_fi * sin(a_fi),
+                (sun_x_c + r_fi * sin(a_fi),
                 sun_y_c - r_fi * cos(a_fi))], color, widthBBox)
+    
     # print(im_SUN)
     # im_SUN.show()
     
