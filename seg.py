a8333e979e0dad9339100ebc43bbd0dc9a3ecd97
GlazmanKonstantin <glk01012020@gmail.com>
2024-12-23 13:00:56 +0400

    Update seg.py

--- seg.py
+++ seg.py
@@ -6,16 +6,30 @@ import sys
 import astropy
 from astropy.io import fits
 
+from converter import FitsConverter
 from math import pi, degrees, sin, cos
 
-def make_seg(color = "e", PATH_LINE_TXT, PATH_SUN, dR, RSUN, sun_x_c, sun_y_c):
-    widthBBox = 1
-    
-    with open(PATH_LINE_TXT) as txt_LINE:
+PATH = r'C:\Users\user\Downloads\test\aia.lev1_euv_12s.2021-01-01T000610Z.171.image_lev1.fits'
+
+PATH_OUTPUT_SUN = r'C:\Users\user\Downloads'
+PATH_FITS = r'C:\Users\user\Downloads\test'
+PATH_CSV = r'C:\Users\user\Downloads\test\trash.csv'
+PATH_IMG = r'C:\Users\user\Downloads\test1'
+PATH_PREDICT_YOLO = r'C:\Users\user\Downloads\2021-01-04T050010Z_3.txt'
+
+def make_seg(yolo_predict, fits_file, color = "red"):
+    FitsConverter(PATH_IMG, PATH_CSV, '', PATH_FITS, verbose=False).convert()
+    im = Image.open(r'C:\Users\user\Downloads\test1\aia.lev1_euv_12s.2021-01-01T000610Z.171.image_lev1_boundary.png').convert('RGB')
+    len_line = im.size[0]
+    widthBBox = 2
+    head = fits_file[1].header
+    RSUN = head["R_SUN"]
+    sun_x_c = head["CRPIX1"]
+    sun_y_c = head["CRPIX2"]
+    with open(yolo_predict) as txt_LINE:
         LINE = txt_LINE.readline()
         
         X_center, Y_center, W, H = [float(x) for x in LINE.split()[1:]]
-        
         x0 = X_center - W / 2
         x1 = X_center + W / 2
         
@@ -29,40 +43,36 @@ def make_seg(color = "e", PATH_LINE_TXT, PATH_SUN, dR, RSUN, sun_x_c, sun_y_c):
         
         y0 = Y_center - H / 2
         y1 = Y_center + H / 2
-        y_st = y0 * dR
-        y_fi = y1 * dR
+
+        y_st = y0
+        y_fi = y1
         
         r_st = RSUN + y_st
         r_fi = RSUN + y_fi
+     
+    im_SUN= Image.fromarray(fits_file[1].data).convert('RGB')
+    draw = ImageDraw.Draw(im_SUN)
     
+    #print(im_SUN.size)
     
-    with Image.open(PATH_SUN) as im_SUN:
-        draw = ImageDraw.Draw(im_SUN)
-        
-        print(im_SUN.size)
-        
-        draw.arc((sun_x_c - r_st, sun_y_c - r_st, sun_x_c + r_st, sun_y_c + r_st), degrees(a_st), degrees(a_fi), color, widthBBox)
-        draw.arc((sun_x_c - r_fi, sun_y_c - r_fi, sun_x_c + r_fi, sun_y_c + r_fi), degrees(a_st), degrees(a_fi), color, widthBBox)
-        
-        a_st += pi / 2
-        a_fi += pi / 2
-        draw.line([(sun_x_c + r_st * sin(a_st),
-                    sun_y_c - r_st * cos(a_st)),
-                   (sun_x_c + r_fi * sin(a_st),
-                    sun_y_c - r_fi * cos(a_st)) ], color, widthBBox)
-        draw.line([(sun_x_c + r_st * sin(a_fi),
-                    sun_y_c - r_st * cos(a_fi)),
-                   (sun_x_c + r_fi * sin(a_fi),
-                    sun_y_c - r_fi * cos(a_fi))], color, widthBBox)
-        
-        
-        im_SUN.save(PATH_SUN)
-        
-def seg_on_SUN(color, fits_file):
-    # Get fits and make .png of SUN with BBox
-    # TODO Get all info from fits and call make_set() function
+    draw.arc((sun_x_c - r_st, sun_y_c - r_st, sun_x_c + r_st, sun_y_c + r_st), degrees(a_st), degrees(a_fi), color, widthBBox)
+    draw.arc((sun_x_c - r_fi, sun_y_c - r_fi, sun_x_c + r_fi, sun_y_c + r_fi), degrees(a_st), degrees(a_fi), color, widthBBox)
+    
+    a_st += pi / 2
+    a_fi += pi / 2
+    draw.line([(sun_x_c + r_st * sin(a_st),
+                sun_y_c - r_st * cos(a_st)),
+                (sun_x_c + r_fi * sin(a_st),
+                sun_y_c - r_fi * cos(a_st)) ], color, widthBBox)
+    draw.line([(sun_x_c + r_st * sin(a_fi),
+                sun_y_c - r_st * cos(a_fi)),
+                (sun_x_c + r_fi * sin(a_fi),
+                sun_y_c - r_fi * cos(a_fi))], color, widthBBox)
+    
+    print(im_SUN)
+    im_SUN.show()
     
 
 if __name__ == "__main__":
-    color = input()
-    make_seg(color)
+    fits_file=fits.open(PATH)
+    make_seg(PATH_PREDICT_YOLO, fits_file)
