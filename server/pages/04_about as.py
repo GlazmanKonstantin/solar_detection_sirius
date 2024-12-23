8979eb1011ff0ecf3795fcecb74f30349ecc43ee
Myagkohod Ekaterina Valeryevna <myagkokhod07@mail.ru>
2024-12-23 19:51:50 +0300

    Update 04_about as.py

--- server/pages/04_about as.py
+++ server/pages/04_about as.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 st.snow()
 st.markdown("# About as")
+st.markdown('Our idea was to train a neural network to detect solar structures and '
+            'assemble a service to track the Sun in real time. '
+            'This website is a visualization of our work.')
+st.markdown('Team: Glazman Konstantin, Selivanov Alexander, Myagkokhod Ekaterina, Elian Andrey')
+st.markdown('Mentor: Blumenau Mark.')
+
+# st.markdown('Наша идея заключалась в том, чтобы обучить нейронную сеть для детекции солнечных структур и '
+#             'собрать сервис для отслеживания Солнца в реальном времени. '
+#             'Этот сайт - визуализация нашей работы. '
+#             'Команда: Глазман Константин, Селиванов Александр, Мягкоход Екатерина, Элиан Андрей '
+#             'Ментор: Блуменау Марк.')
